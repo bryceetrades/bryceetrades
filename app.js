a566b712dc3a4666907655211e8fcd80a6738573
bryceetrades <kimsmercy496@gmail.com>
2026-07-01 09:55:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const loginBtn = document.getElementById("loginBtn");
 
+// Your Deriv App ID
+const APP_ID = "33zqFdSUnH9jY0bjdm8Vn";
+
 loginBtn.addEventListener("click", () => {
-    alert("Login feature coming next!");
+
+    const redirect = encodeURIComponent(window.location.origin);
+
+    const url =
+    `https://oauth.deriv.com/oauth2/authorize?app_id=${APP_ID}&l=EN&redirect_uri=${redirect}`;
+
+    window.location.href = url;
+
 });
