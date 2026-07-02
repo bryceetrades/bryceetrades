7df025ba814c0794aeeb3ff8104e69feb28491a8
Brycee <kimsmercy496@gmail.com>
2026-07-02 23:21:13 +0300

    update auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,47 @@
 // auth.js
 
+const AUTH_URL = "https://oauth.deriv.com/oauth2/authorize";
+
+function randomString(length) {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~";
+    let result = "";
+
+    const array = new Uint8Array(length);
+    crypto.getRandomValues(array);
+
+    for (let i = 0; i < length; i++) {
+        result += chars[array[i] % chars.length];
+    }
+
+    return result;
+}
+
+async function sha256(text) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(text);
+    return await crypto.subtle.digest("SHA-256", data);
+}
+
+function base64url(buffer) {
+    return btoa(String.fromCharCode(...new Uint8Array(buffer)))
+        .replace(/\+/g, "-")
+        .replace(/\//g, "_")
+        .replace(/=/g, "");
+}
+
 async function login() {
-    alert("OAuth 2.0 login will be implemented here.");
+    const verifier = randomString(64);
+    localStorage.setItem("pkce_verifier", verifier);
+
+    const challenge = base64url(await sha256(verifier));
+
+    const url =
+        `${AUTH_URL}?app_id=${CONFIG.APP_ID}` +
+        `&redirect_uri=${encodeURIComponent(CONFIG.REDIRECT_URI)}` +
+        `&response_type=code` +
+        `&code_challenge=${challenge}` +
+        `&code_challenge_method=S256` +
+        `&scope=read`;
+
+    window.location.href = url;
 }
