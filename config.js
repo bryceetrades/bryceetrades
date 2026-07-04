91bc2538958b52e32385eb74d3364a4ee45aa9ed
Brycee <kimsmercy496@gmail.com>
2026-07-04 21:27:51 +0300

    update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
-    APP_ID: "33zqFdSUnH9jY0bjdm8Vn",
+    CLIENT_ID: "33zqFdSUnH9jY0bjdm8Vn",
     REDIRECT_URI: "https://bryceetrades-kimsmercy496-2389s-projects.vercel.app",
-    SYMBOL: "R_100"
+    SYMBOL: "R_100",
+    WS_APP_ID: "1089"
 };
